3fd36edb2c2dc3bf938d10cba979a3d98964695f
ThatBadName <thatbadname@outlook.com>
2025-08-06 12:07:07 +0100

    added label to message contenr box

--- app/dashboard/[id]/messages/[msg]/EditorV1.tsx
+++ app/dashboard/[id]/messages/[msg]/EditorV1.tsx
@@ -16,13 +16,16 @@ export function EditorV1({
 }) {
   return (
     <div>
-      <TextareaInput
-        value={message.content ?? ""}
-        maxLength={2000}
-        onChange={(v) =>
-          setMessage({ ...message, content: v.target.value || null })
-        }
-      />
+      <div>
+        <p className="font-semibold">Message Content</p>
+        <TextareaInput
+          value={message.content ?? ""}
+          maxLength={2000}
+          onChange={(v) =>
+            setMessage({ ...message, content: v.target.value || null })
+          }
+        />
+      </div>
     </div>
   );
 }
